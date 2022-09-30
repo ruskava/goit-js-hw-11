c5345a90f4856e6d97a8171b178a492e8b0e8ed2
ruskava <94966888+ruskava@users.noreply.github.com>
2022-09-30 12:28:10 +0300

    2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
+
+
+
