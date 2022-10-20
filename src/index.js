f2900366a7a9ba04fdea857559bd1ab47bd69c40
ruskava <94966888+ruskava@users.noreply.github.com>
2022-10-20 17:34:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 
 import './css/styles.css';
+import photoApiService from './fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from 'axios';
 
+
+
+const refs = {
+    form: document.querySelector("#search-form"),
+    searchBtnInput: document.querySelector("input"),
+    gallery: document.querySelector(".gallery"),
+    loadMore: document.querySelector(".load-more")
+}
+
+const PhotoApiService = new PhotoApiService();
+
+refs.form.addEventListener("submit", onSearch);
+
+function onSearch(e) { 
+    e.preventDefault();
+
+photoApiService.query = e.currentTarget.elements.searchQuery.value;
+photoApiService.resetPage();
+refs.gallery.innerHTML = "";
+}
 
 
